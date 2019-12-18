8848b5b0ffa0d26bd8e17e34843299c704946a01
renu0110 <renukabandla03@gmail.com>
2019-12-18 08:21:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-aceknd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-aceknd)
---
+++ index.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+function Welcome(props) {
+  return <h1>Hello, {props.name} Age {props.age}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" age="24" />
+      <Welcome name="Cahal"  age="24"/>
+      <Welcome name="Edite" age="24"/>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);

af5850d581d39ca9e2b3efc9f8d0e44dffa2f158
renu0110 <renukabandla03@gmail.com>
2019-12-19 11:49:33 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,20 +6,19 @@ import Hello from './Hello';
 import './style.css';
 
 function Welcome(props) {
-  return <h1>Hello, {props.name} Age {props.age}</h1>;
+  return<h1>Hello, {props.name} Age {props.age}</h1>;
 }
 
-function App() {
-  return (
-    <div>
+var App =(
+  <div>
       <Welcome name="Sara" age="24" />
       <Welcome name="Cahal"  age="24"/>
       <Welcome name="Edite" age="24"/>
     </div>
-  );
-}
+);
+    
 
 ReactDOM.render(
-  <App />,
+ App,
   document.getElementById('root')
 );
